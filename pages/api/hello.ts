7b2c44d66b65c31589dd7943a92a3c5c24d9939b
Marcus Plienegger <mplienegger@exchangesolutions.com>
2021-04-23 09:21:34 -0400

    Added typescript support

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,8 +1,10 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
 /*
  API Routes let you create an API endpoint inside a Next.js app. You can do so by creating a 
  function inside the pages/api directory that has the following format:
 */
 
-export default function handler(req, res) {
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
   res.status(200).json({ text: "Hello" });
 }
